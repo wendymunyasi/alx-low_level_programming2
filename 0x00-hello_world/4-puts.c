f1eae6fc6507a84d09a7d4fabe968f635197a210
root <wendymunyasi@gmail.com>
2022-04-11 04:31:12 -0700

    Add a C program that prints something

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Always 0 on success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle);
+
+	return(0);
+
+}
